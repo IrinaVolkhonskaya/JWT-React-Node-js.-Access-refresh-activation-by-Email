ded15b3cd742ea7cda0d0b63754c70d76f3d8ac5
IRINA VOLKHONSKAYA <irina.volhonskaya@gmail.com>
2024-01-11 13:18:55 +0100

    add logic for user refresh token

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -56,8 +56,13 @@ class UserController {
     }
     async refresh(req, res, next) {
         try {
-
-        } catch (e) { }
+            const { refreshToken } = req.cookies;
+            const userData = await userService.refresh(refreshToken);
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 100, httpOnly: true })
+            return res.json(userData);
+        } catch (e) {
+            next(e);
+        }
     }
     async getUsers(req, res, next) {
         try {
